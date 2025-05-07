6a41d7e8aa497df2139b30047049634c8d3b4c4d
versiw <stao.ding@qq.com>
2025-05-07 21:20:35 +0800

    feat: 添加 Umami 网站统计 script

--- .vitepress/configs/head.config.ts
+++ .vitepress/configs/head.config.ts
@@ -4,6 +4,36 @@ const headConfig = (env): HeadConfig[] => {
   return [
     ['link', { rel: 'icon', href: env.VITE_BASE + 'versiw.ico' }],
     ['meta', { name: 'msvalidate.01', content: '7D0D5E34E0E1AE2E7F92802CB2D9308D' }],
+    // Umami docs.versiw.com
+    [
+      'script',
+      {
+        defer: '',
+        async: '',
+        src: 'https://umami.versiw.com/script.js',
+        'data-website-id': 'c50730fb-e245-4518-b757-44e58d397989'
+      }
+    ],
+    // Umami versiw.netlify.app
+    [
+      'script',
+      {
+        defer: '',
+        async: '',
+        src: 'https://umami.versiw.com/script.js',
+        'data-website-id': 'cc8a7cdd-4864-4cd3-a374-9051c5064714'
+      }
+    ],
+    // Umami versiw.github.io
+    [
+      'script',
+      {
+        defer: '',
+        async: '',
+        src: 'https://umami.versiw.com/script.js',
+        'data-website-id': 'f516de8c-ed2a-483b-b34b-7d7760a7586d'
+      }
+    ],
     [
       'link',
       {
