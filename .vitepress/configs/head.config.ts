e9e79fcc549d110ff2c49faf8288739a0cb1f3f1
versiw <stao.ding@qq.com>
2025-05-09 19:36:40 +0800

    refactor: 优化 Umami 托管跟踪脚本

--- .vitepress/configs/head.config.ts
+++ .vitepress/configs/head.config.ts
@@ -1,29 +1,8 @@
 import type { HeadConfig } from 'vitepress'
 
-const headConfig = (env): HeadConfig[] => {
-  return [
+const headConfig = (env) => {
+  const heads: HeadConfig[] = [
     ['link', { rel: 'icon', href: env.VITE_BASE + 'versiw.ico' }],
-    ['meta', { name: 'msvalidate.01', content: '7D0D5E34E0E1AE2E7F92802CB2D9308D' }],
-    // Umami docs.versiw.com
-    [
-      'script',
-      {
-        defer: '',
-        async: '',
-        src: 'https://umami.versiw.com/script.js',
-        'data-website-id': 'c50730fb-e245-4518-b757-44e58d397989'
-      }
-    ],
-    // Umami versiw.github.io
-    [
-      'script',
-      {
-        defer: '',
-        async: '',
-        src: 'https://umami.versiw.com/script.js',
-        'data-website-id': 'f516de8c-ed2a-483b-b34b-7d7760a7586d'
-      }
-    ],
     [
       'link',
       {
@@ -35,6 +14,37 @@ const headConfig = (env): HeadConfig[] => {
       }
     ]
   ]
+
+  if (process.env.NODE_ENV === 'production') {
+    heads.push(
+      // Bing Webmaster Tools
+      ['meta', { name: 'msvalidate.01', content: '7D0D5E34E0E1AE2E7F92802CB2D9308D' }],
+      // Umami docs.versiw.com
+      [
+        'script',
+        {
+          defer: '',
+          async: '',
+          src: env.VITE_BASE + 'scripts/umm.js',
+          'data-host-url': 'https://umami.versiw.com',
+          'data-website-id': 'c50730fb-e245-4518-b757-44e58d397989'
+        }
+      ],
+      // Umami versiw.github.io
+      [
+        'script',
+        {
+          defer: '',
+          async: '',
+          src: env.VITE_BASE + 'scripts/umm.js',
+          'data-host-url': 'https://umami.versiw.com',
+          'data-website-id': 'f516de8c-ed2a-483b-b34b-7d7760a7586d'
+        }
+      ]
+    )
+  }
+
+  return heads
 }
 
 export default headConfig
