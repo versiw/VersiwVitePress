8ee560198da250723355ccbc4dc578967128e619
versiw <stao.ding@qq.com>
2025-05-12 21:03:42 +0800

    fix: 优化 Umami 配置文件 production 模式下构建

--- .vitepress/configs/head.config.ts
+++ .vitepress/configs/head.config.ts
@@ -16,10 +16,11 @@ const headConfig = (env) => {
   ]
 
   if (process.env.NODE_ENV === 'production') {
+    const isGitHubPages = env.VITE_DEPLOY_PLATFORM === 'github'
     heads.push(
       // Bing Webmaster Tools
       ['meta', { name: 'msvalidate.01', content: '7D0D5E34E0E1AE2E7F92802CB2D9308D' }],
-      // Umami docs.versiw.com
+      // Umami Analysis
       [
         'script',
         {
@@ -27,18 +28,9 @@ const headConfig = (env) => {
           async: '',
           src: env.VITE_BASE + 'scripts/umm.js',
           'data-host-url': 'https://umami.versiw.com',
-          'data-website-id': 'c50730fb-e245-4518-b757-44e58d397989'
-        }
-      ],
-      // Umami versiw.github.io
-      [
-        'script',
-        {
-          defer: '',
-          async: '',
-          src: env.VITE_BASE + 'scripts/umm.js',
-          'data-host-url': 'https://umami.versiw.com',
-          'data-website-id': 'f516de8c-ed2a-483b-b34b-7d7760a7586d'
+          'data-website-id': isGitHubPages
+            ? 'f516de8c-ed2a-483b-b34b-7d7760a7586d'
+            : 'c50730fb-e245-4518-b757-44e58d397989'
         }
       ]
     )
